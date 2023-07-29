8859404af08594ad7c131e635f8c52b3c82857e1
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-29 17:00:34 +0200

    (feat): testing getting players

--- allsvenskan_cards/src/App.tsx
+++ allsvenskan_cards/src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import React from "react";
+import { getAllPlayers } from "./services/playerServices";
+import { IPlayer } from "./models/IPlayer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [players, setPlayers] = useState<IPlayer[]>([]);
+  useEffect(() => {
+    getAllPlayers().then((players) => {
+      setPlayers(players);
+    });
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {players.map((player) => (
+        <div key={player._id}>
+          <h3>{player.name}</h3>
+          <p>{player.club}</p>
+          <img src={player.imgURL} alt={player.name} />
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
