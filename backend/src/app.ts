9a9f9d3ee4105d49ff287e8de7ee88faa655d359
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-29 14:54:55 +0200

    (feat): create backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,20 @@ import cors from "cors";
 const app = express();
 app.use(json());
 
-app.listen(4000, () => {
-  console.log("Server running on port 4000!");
-});
+const port = process.env.PORT || 5000;
+async function run() {
+  try {
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(
+      process.env.MONGO_CONNECTION_STRING as string
+    );
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
