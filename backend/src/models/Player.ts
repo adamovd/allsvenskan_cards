61b00a2e33a08527f60e1822b15f969ded65d7ac
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-29 14:55:45 +0200

    (feat):  mongoose schema for player

---
+++ backend/src/models/Player.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const PlayerSchema = new mongoose.Schema({
+  imgURL: {
+    type: String,
+  },
+  name: {
+    type: String,
+  },
+  number: {
+    type: String,
+  },
+  born: {
+    type: String,
+  },
+  age: {
+    type: String,
+  },
+  nationality: {
+    type: String,
+  },
+  position: {
+    type: String,
+  },
+  club: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Player", PlayerSchema);
