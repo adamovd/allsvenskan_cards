792fe76451e01b48ca5eda01bc8717ce2857ba39
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-31 01:52:15 +0200

    (feat): style for players name

---
+++ allsvenskan_cards/src/components/styled/PlayerName.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const PlayerName = styled.h1`
+  text-align: center;
+  color: black;
+  font-size: 1.2rem;
+  font-family: Arial, Helvetica, sans-serif;
+`;
+
+export default PlayerName;
