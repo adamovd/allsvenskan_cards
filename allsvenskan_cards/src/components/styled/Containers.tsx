ed194ddcf5cc9e9cc8040b9cb20172e9df7dcb60
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-31 01:51:38 +0200

    (feat): adding styles for containers

---
+++ allsvenskan_cards/src/components/styled/Containers.tsx
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 370px;
+  height: 470px;
+  border-radius: 6px;
+  background-color: #ffffffee;
+  margin: 20px;
+`;
+
+export const BottomContainer = styled.article`
+  width: 300px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const InfoContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
