077279cc852429859a7ecf3376b46cb1b7350b9a
Adam Danielsson <adam.ov.danielsson@gmail.com>
2023-07-31 01:53:21 +0200

    (feat): add flags to nationality on players

--- backend/seedDb/players.ts
+++ backend/seedDb/players.ts
@@ -6,7 +6,8 @@ exports.PlayerData = [
     number: "15",
     born: "1989-06-23",
     age: "34",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Goalkeeper",
     club: "AIK",
   },
@@ -17,7 +18,8 @@ exports.PlayerData = [
     number: "23",
     born: "1989-10-21",
     age: "33",
-    nationality: "Serbien",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/RS.svg",
     position: "Goalkeeper",
     club: "AIK",
   },
@@ -28,7 +30,8 @@ exports.PlayerData = [
     number: "4",
     born: "1990-09-05",
     age: "32",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -39,7 +42,8 @@ exports.PlayerData = [
     number: "5",
     born: "1992-01-30",
     age: "31",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -50,18 +54,20 @@ exports.PlayerData = [
     number: "6",
     born: "1996-09-14",
     age: "26",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Försvarare",
     club: "AIK",
   },
   {
     imgURL:
-      "https://d281ovntc614pi.cloudfront.net/content/uploads/2023/07/BB230621JW0434-600x900.jpg",
+      "https://d281ovntc614pi.cloudfront.net/content/uploads/2023/03/Axel-Bjo%CC%88rnstro%CC%88m-600x871.jpg",
     name: "Axel Björnström",
     number: "12",
     born: "1995-09-10",
     age: "27",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -72,7 +78,8 @@ exports.PlayerData = [
     number: "16",
     born: "2002-03-16",
     age: "21",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -83,7 +90,8 @@ exports.PlayerData = [
     number: "17",
     born: "1997-06-27",
     age: "26",
-    nationality: "Danmark",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/DK.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -94,7 +102,8 @@ exports.PlayerData = [
     number: "25",
     born: "1996-09-27",
     age: "26",
-    nationality: "Kenya",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/KE.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -105,7 +114,8 @@ exports.PlayerData = [
     number: "32",
     born: "1999-10-07",
     age: "23",
-    nationality: "Portugal",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/PT.svg",
     position: "Försvarare",
     club: "AIK",
   },
@@ -115,7 +125,8 @@ exports.PlayerData = [
     number: "7",
     born: "1996-04-12",
     age: "27",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -126,7 +137,8 @@ exports.PlayerData = [
     number: "8",
     born: "2000-04-22",
     age: "23",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -137,7 +149,8 @@ exports.PlayerData = [
     number: "10",
     born: "1989-03-22",
     age: "34",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -148,7 +161,8 @@ exports.PlayerData = [
     number: "14",
     born: "1998-04-06",
     age: "25",
-    nationality: "Nigeria",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/NG.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -159,7 +173,8 @@ exports.PlayerData = [
     number: "19",
     born: "1994-01-31",
     age: "29",
-    nationality: "Bosnien & Hercegovina",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/BA.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -170,7 +185,8 @@ exports.PlayerData = [
     number: "20",
     born: "1998-04-05",
     age: "25",
-    nationality: "Irland",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/IE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -181,7 +197,8 @@ exports.PlayerData = [
     number: "21",
     born: "2000-04-21",
     age: "23",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -192,7 +209,8 @@ exports.PlayerData = [
     number: "22",
     born: "1997-11-05",
     age: "25",
-    nationality: "Elfenbenskusten",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/CI.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -203,7 +221,8 @@ exports.PlayerData = [
     number: "43",
     born: "2004-10-22",
     age: "18",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -214,7 +233,8 @@ exports.PlayerData = [
     number: "45",
     born: "2005-05-10",
     age: "18",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Mittfältare",
     club: "AIK",
   },
@@ -225,7 +245,8 @@ exports.PlayerData = [
     number: "9",
     born: "2002-03-09",
     age: "21",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Anfallare",
     club: "AIK",
   },
@@ -236,7 +257,8 @@ exports.PlayerData = [
     number: "11",
     born: "1992-04-15",
     age: "31",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Anfallare",
     club: "AIK",
   },
@@ -247,7 +269,8 @@ exports.PlayerData = [
     number: "47",
     born: "2004-02-29",
     age: "19",
-    nationality: "Sverige",
+    nationality:
+      "https://catamphetamine.gitlab.io/country-flag-icons/3x2/SE.svg",
     position: "Anfallare",
     club: "AIK",
   },
